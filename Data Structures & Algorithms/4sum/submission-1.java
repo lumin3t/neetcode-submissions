b201e67f08ffee262adb9f896c6620d7b8465cf9
lumin3t <134595525+lumin3t@users.noreply.github.com>
2026-07-06 21:43:12 +0530

    Add: 4sum - submission-1

---
+++ Data Structures & Algorithms/4sum/submission-1.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+         // 2 pointer two fixed nums
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        if (nums == null || nums.length < 4) {
+            return res;
+        }
+        Arrays.sort(nums);
+        for (int i=0; i<n-3; i++) {
+            if (i>0 && nums[i]==nums[i-1]) {
+                continue; // skip dupes
+            }
+            for (int j=i+1; j<n-2; j++) {
+                if (j>i+1 && nums[j]==nums[j-1]) {
+                    continue;
+                }
+                int l=j+1, r=n-1;
+                while(l<r) {
+                    long sum = (long)nums[i]+nums[j]+nums[l]+nums[r];
+                    if (sum == target) {
+                        List<Integer> quad = Arrays.asList(nums[i], nums[j], nums[l], nums[r]);
+                        if (!res.contains(quad)) res.add(quad);
+                        while (l<r && nums[l]== nums[l+1]) {
+                            l++;
+                        }
+                        while(l<r && nums[r]==nums[r-1]) {
+                            r--;
+                        }
+                        l++;
+                        r--;
+                    } else if (sum<target) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
