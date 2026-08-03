4bdc741a61b600fa254ee397fe8cbf1c5f08aed4
lumin3t <134595525+lumin3t@users.noreply.github.com>
2026-08-03 10:18:37 +0530

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+          if (s == null) {
+            return false; 
+        }
+        s = s.toLowerCase(); 
+        s = s.replaceAll("[^a-zA-Z0-9]", ""); // Remove non-letter non-number characters
+        if (s.isEmpty()) {
+            return true; 
+        }
+        int n = s.length()-1;
+        for (int i=0; i<=n/2; i++) {
+            if (s.charAt(i) != s.charAt(n-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
