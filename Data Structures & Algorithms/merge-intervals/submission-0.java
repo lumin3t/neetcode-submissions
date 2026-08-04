07e40fabad930e7a5a31ba7280a07c52baeebeff
lumin3t <134595525+lumin3t@users.noreply.github.com>
2026-08-04 20:53:48 +0530

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals == null || intervals.length<=1) return intervals;
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        List<int[]> res = new ArrayList<>();
+        int[] newInterval = intervals[0];
+        res.add(newInterval);
+        for (int[] interval: intervals) {
+            if (interval[0] <= newInterval[1]) {
+                newInterval[1] = Math.max(interval[1], newInterval[1]);
+            } else {
+                newInterval = interval;
+                res.add(newInterval);
+            }
+        }
+        return res.toArray(new int[res.size()][]); 
+    }
+}
