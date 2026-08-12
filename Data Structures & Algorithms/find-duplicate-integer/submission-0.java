1eb40515a08c384bf46c3d003449dd55925806d8
lumin3t <134595525+lumin3t@users.noreply.github.com>
2026-08-12 11:26:58 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int num:nums) {
+            if (!set.add(num)) return num;
+        }
+        return -1;
+    }
+}
